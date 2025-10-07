78ef3b9ef19ae00eea5814f69ff787bfca9104c0
Raymart Reyes <165752884+reyesraymart05@users.noreply.github.com>
2025-10-07 11:06:29 -0700

    Time: 2 ms (74.47%), Space: 66.5 MB (89.19%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cs
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cs
@@ -1,25 +1,21 @@
 public class Solution {
     public int[] ProductExceptSelf(int[] nums) {
-        int[] answers = Enumerable.Repeat(1, nums.Length).ToArray();
-        
-        for (int i = 0; i < nums.Length; i++) {
-            if (i == 0) {
-                answers[i] = 1;
-            }
-            if (i - 1> -1) {
-                var pre = answers[i -1] * nums[i - 1];
-                answers[i] *= pre;
-            }
-        }
-        var suf = 1;
-        for (int i = nums.Length -1 ; i >= 0; i --) {
+        int n = nums.Length;
+        int[] result = new int[n];
 
-                answers[i] *= suf;
-                suf *= nums[i];
-            
+        result[0] = 1;
+        for (int i = 1; i < n; i++){
+            result[i] = result[i-1] * nums[i-1];
+
         }
-        
-        return answers;
+
+        int rightProduct = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] = result[i] * rightProduct;
+            rightProduct *= nums[i];
+        }
+
+        return result;
 
     }
 }
