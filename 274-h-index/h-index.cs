d87c2aa751815b65cffd2fdbe72cc98e3f09d74b
Raymart Reyes <165752884+reyesraymart05@users.noreply.github.com>
2025-10-18 15:38:10 -0700

    Time: 3 ms (69.42%) | Memory: 42.1 MB (49.21%) - LeetSync

---
+++ 274-h-index/h-index.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int HIndex(int[] citations) {
+        Array.Sort(citations);
+        Array.Reverse(citations);
+
+        int h = 0;
+        for (int i = 0; i < citations.Length; i++) {
+            if (citations[i] >= i + 1) {
+                h = i + 1;
+            } else {
+                break;
+            }
+        }
+        return h;
+    }
+}
