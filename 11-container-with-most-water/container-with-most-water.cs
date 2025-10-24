1faf5ca3cdfdf61b10b69c095b78e335e65cb9f4
Raymart Reyes <165752884+reyesraymart05@users.noreply.github.com>
2025-10-23 19:46:49 -0700

    Time: 1 ms (99.87%) | Memory: 63.4 MB (41.23%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int MaxArea(int[] height) {
+        int left = 0;
+        int right = height.Length - 1;
+        int maxArea = 0;
+
+        while (left < right)
+        {
+            int area = Math.Min(height[left], height[right]) * (right - left);
+            if (area > maxArea) maxArea = area;
+
+            if (height[left] <= height[right]) left++;
+            else right--;
+        }
+
+        return maxArea;
+    }
+}
