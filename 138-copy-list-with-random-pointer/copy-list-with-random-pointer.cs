19d9ec15f3851c84abb89c3e38ec137c1a3877dc
Raymart Reyes <165752884+reyesraymart05@users.noreply.github.com>
2025-11-14 18:13:43 -0700

    Time: 59 ms (99.48%) | Memory: 42.4 MB (98.10%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cs
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cs
@@ -1,22 +1,31 @@
 public class Solution {
     public Node CopyRandomList(Node head) {
-        if (head == null) return null;
-        
-        Dictionary<Node, Node> oldToNew = new Dictionary<Node, Node>();
-        
-        Node curr = head;
-        while (curr != null) {
-            oldToNew[curr] = new Node(curr.val);
-            curr = curr.next;
+        if(head==null) return null;
+        Node current=head;
+        while(current!=null){
+            Node newnode=new Node(current.val);
+            newnode.next=current.next;
+            current.next=newnode;
+            current=newnode.next;
         }
-        
-        curr = head;
-        while (curr != null) {
-            oldToNew[curr].next = curr.next != null ? oldToNew[curr.next] : null;
-            oldToNew[curr].random = curr.random != null ? oldToNew[curr.random] : null;
-            curr = curr.next;
+        current=head;
+        while(current!=null){
+            if(current.random!=null){
+                current.next.random=current.random.next;
+            }
+            current=current.next.next;
         }
-        
-        return oldToNew[head];
+        Node newhead=head.next;
+        current=head;
+        Node newcurrent=newhead;
+        while(current!=null){
+            current.next=newcurrent.next;
+            current=current.next;
+            if(current!=null){
+                newcurrent.next=current.next;
+                newcurrent=current.next;
+            }
+        }
+        return newhead;
     }
 }
