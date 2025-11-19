1f6467410a1738cd5aa3ce006516f22937af6b67
Raymart Reyes <165752884+reyesraymart05@users.noreply.github.com>
2025-11-18 20:09:57 -0700

    Time: 21 ms (99.76%) | Memory: 174.8 MB (44.53%) - LeetSync

--- 146-lru-cache/lru-cache.cs
+++ 146-lru-cache/lru-cache.cs
@@ -1,59 +1,89 @@
-public class LRUCache {
-
-    private readonly int capacity;
-    private readonly Dictionary<int, LinkedListNode<CacheItem>> cacheMap;
-    private readonly LinkedList<CacheItem> cacheList;
-
-    public LRUCache(int capacity) {
-        this.capacity = capacity;
-        cacheMap = new Dictionary<int, LinkedListNode<CacheItem>>(capacity);
-        cacheList = new LinkedList<CacheItem>();
-    }
-    
-    public int Get(int key) {
-        if (cacheMap.TryGetValue(key, out var node))
-        {
-            cacheList.Remove(node);
-            cacheList.AddFirst(node);
-            return node.Value.Value;
-        }
-        return -1;
-    }
-    
-    public void Put(int key, int value) {
-        if (cacheMap.TryGetValue(key, out var node))
-        {
-            node.Value.Value = value;
-            cacheList.Remove(node);
-            cacheList.AddFirst(node);
-        }
-        else
-        {
-            if (cacheMap.Count >= capacity)
-            {
-                var lastNode = cacheList.Last;
-                cacheMap.Remove(lastNode.Value.Key);
-                cacheList.RemoveLast();
-            }
-
-            var newNode = new LinkedListNode<CacheItem>(new CacheItem(key, value));
-            cacheMap.Add(key, newNode);
-            cacheList.AddFirst(newNode);
-        }
-    }
-
-    private class CacheItem
+    public class LRUCache
     {
-        public int Key { get; }
-        public int Value { get; set; }
+        private readonly Dictionary<int, Node> nodes;
+        private readonly int capacity;
+        private Node head;
+        private Node tail;
 
-        public CacheItem(int key, int value)
+        public LRUCache(int capacity)
         {
-            Key = key;
-            Value = value;
+            head = new Node();
+            tail = new Node();
+            head.next = tail;
+            tail.prev = head;
+            this.capacity = capacity;
+            nodes = [];
+        }
+
+        public int Get(int key)
+        {
+            if (nodes.TryGetValue(key, out Node node))
+            {
+                // move this node to head as most recently used
+                Node nodePrev = node.prev;
+                Node nodeNext = node.next;
+                Node headNext = head.next;
+                if (node != headNext)
+                {
+                    nodePrev.next = nodeNext;
+                    nodeNext.prev = nodePrev;
+                    node.next = headNext;
+                    node.prev = head;
+                    head.next = node;
+                    headNext.prev = node;
+                }
+                return node.val;
+            }
+            return -1;
+        }
+
+        public void Put(int key, int value)
+        {
+            if (nodes.TryGetValue(key, out Node node))
+            {
+                // update and move to head
+                node.val = value;
+                Get(key);
+            }
+            else
+            {
+                // check capacity
+                if (nodes.Count >= capacity)
+                {
+                    // evict LRU from tail: update tail.prev and update
+                    Node toOverride = tail.prev;
+                    nodes.Remove(toOverride.key);
+                    toOverride.key = key;
+                    toOverride.val = value;
+                    nodes.Add(toOverride.key, toOverride);
+                    Get(toOverride.key);
+                }
+                else
+                {
+                    // add to head
+                    Node newNode = new Node
+                    {
+                        key = key,
+                        val = value,
+                    };
+                    Node headNext = head.next;
+                    nodes.Add(key, newNode);
+                    newNode.next = headNext;
+                    newNode.prev = head;
+                    headNext.prev = newNode;
+                    head.next = newNode;
+                }
+            }
         }
     }
-}
+
+    class Node
+    {
+        public int key;
+        public int val;
+        public Node prev;
+        public Node next;
+    }
 
 /**
  * Your LRUCache object will be instantiated and called as such:
