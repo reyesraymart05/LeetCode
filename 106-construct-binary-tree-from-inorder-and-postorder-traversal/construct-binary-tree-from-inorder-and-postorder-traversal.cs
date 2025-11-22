727d11c6b7e89ce79f669a0cd55a08a432d1b08a
Raymart Reyes <165752884+reyesraymart05@users.noreply.github.com>
2025-11-21 19:30:58 -0700

    Time: 1 ms (98.02%) | Memory: 44.6 MB (55.94%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cs
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cs
@@ -1,38 +1,26 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     public int val;
- *     public TreeNode left;
- *     public TreeNode right;
- *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 public class Solution {
-  public TreeNode BuildTree(int[] inorder, int[] postorder)
-{
-    if (inorder.Length == 0 || postorder.Length == 0) return null;
-    return BuildTree(inorder, 0, inorder.Length, postorder, 0, postorder.Length);
-}
+    private Dictionary<int, int> inorderIndexMap;
+    private int[] postorder;
 
-private TreeNode BuildTree(int[] inorder, int inorderStart, int inorderEnd, int[] postorder, int postorderStart, int postorderEnd)
-{
-    var root = new TreeNode(postorder[postorderEnd - 1]);
+    public TreeNode BuildTree(int[] inorder, int[] postorder) {
+        this.postorder = postorder;
+        inorderIndexMap = new Dictionary<int, int>();
+        for (int i = 0; i < inorder.Length; i++) {
+            inorderIndexMap[inorder[i]] = i;
+        }
+        return BuildSubtree(0, 0, inorder.Length);
+    }
 
-    var inorderIndex = inorderStart;
-    for (; inorderIndex < inorderEnd; inorderIndex++)
-        if (inorder[inorderIndex] == postorder[postorderEnd - 1])
-            break;
+    private TreeNode BuildSubtree(int inorderStart, int postorderStart, int subtreeSize) {
+        if (subtreeSize <= 0) return null;
 
-    var leftLength = inorderIndex - inorderStart;
-    if (leftLength > 0)
-        root.left = BuildTree(inorder, inorderStart, inorderIndex, postorder, postorderStart, postorderStart + leftLength);
-    if (inorderEnd > inorderIndex + 1)
-        root.right = BuildTree(inorder, inorderIndex + 1, inorderEnd, postorder, postorderStart + leftLength, postorderEnd - 1);
+        int rootVal = postorder[postorderStart + subtreeSize - 1];
+        int rootIndexInorder = inorderIndexMap[rootVal];
+        int leftSubtreeSize = rootIndexInorder - inorderStart;
 
-    return root;
-}
+        TreeNode leftChild = BuildSubtree(inorderStart, postorderStart, leftSubtreeSize);
+        TreeNode rightChild = BuildSubtree(rootIndexInorder + 1, postorderStart + leftSubtreeSize, subtreeSize - leftSubtreeSize - 1);
+
+        return new TreeNode(rootVal, leftChild, rightChild);
+    }
 }
