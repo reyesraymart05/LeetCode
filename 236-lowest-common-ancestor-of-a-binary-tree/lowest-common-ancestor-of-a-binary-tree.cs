4431b23a4546b1275ed38b9c4787de16afac560e
Raymart Reyes <165752884+reyesraymart05@users.noreply.github.com>
2025-11-24 19:00:25 -0700

    Time: 95 ms (57.96%) | Memory: 47.3 MB (42.63%) - LeetSync

---
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = LowestCommonAncestor(root.left, p, q);
+        TreeNode right = LowestCommonAncestor(root.right, p, q);
+
+        return left != null && right != null ? root : left ?? right;
+    }
+}
