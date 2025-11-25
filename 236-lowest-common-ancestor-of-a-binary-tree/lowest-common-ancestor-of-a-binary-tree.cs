1bacbebc20836777e40be26cdac004e7916d8047
Raymart Reyes <165752884+reyesraymart05@users.noreply.github.com>
2025-11-24 19:01:19 -0700

    Time: 84 ms (94.25%) | Memory: 47.3 MB (33.41%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cs
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cs
@@ -1,11 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
 public class Solution {
-    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
-    {
-        if (root == null || root.val == p.val || root.val == q.val) return root;
+    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root is null) return null;
 
-        TreeNode left = LowestCommonAncestor(root.left, p, q);
-        TreeNode right = LowestCommonAncestor(root.right, p, q);
+        if(root == q || root == p) return root;
 
-        return left != null && right != null ? root : left ?? right;
+        var left = LowestCommonAncestor(root.left, p, q);
+        var right = LowestCommonAncestor(root.right, p, q);
+
+        if(left is not null && right is not null) return root;
+
+        if(left is not null) return left;
+        if(right is not null) return right;
+
+        return null;
     }
 }
