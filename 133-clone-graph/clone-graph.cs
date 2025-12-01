b0321dfe05fa62537e14469c4220a11c20f0aaaf
Raymart Reyes <165752884+reyesraymart05@users.noreply.github.com>
2025-12-01 14:36:39 -0700

    Time: 122 ms (89.78%) | Memory: 48.1 MB (23.93%) - LeetSync

--- 133-clone-graph/clone-graph.cs
+++ 133-clone-graph/clone-graph.cs
@@ -1,28 +1,39 @@
+/*
+// Definition for a Node.
+public class Node {
+    public int val;
+    public IList<Node> neighbors;
+
+    public Node() {
+        val = 0;
+        neighbors = new List<Node>();
+    }
+
+    public Node(int _val) {
+        val = _val;
+        neighbors = new List<Node>();
+    }
+
+    public Node(int _val, List<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
 public class Solution {
+    private Dictionary<Node, Node> map = new();
+
     public Node CloneGraph(Node node) {
-        if (node is null) return null;
-        var queue = new Queue<Node>();
-        var visited = new Dictionary<Node, (Node, bool)>();
-        queue.Enqueue(node);
-        visited.Add(node, (new Node(node.val), false));
+        if (node == null) return null;
+        if (map.ContainsKey(node)) return map[node];
 
-        while (queue.Any()) {
-            var current = queue.Dequeue();
-            var copy = visited[current].Item1;
+        var clone = new Node(node.val);
+        map[node] = clone;
 
-            foreach (var neighbor in current.neighbors) {
-                if (!visited.ContainsKey(neighbor))
-                    visited.Add(neighbor, (new Node(neighbor.val), false));
+        foreach (var neighbor in node.neighbors)
+            clone.neighbors.Add(CloneGraph(neighbor));
 
-                copy.neighbors.Add(visited[neighbor].Item1);
-
-                if (!visited[neighbor].Item2 && !queue.Contains(neighbor))
-                    queue.Enqueue(neighbor);
-            }
-
-            visited[current] = (visited[current].Item1, true);
-        }
-
-        return visited[node].Item1;
+        return clone;
     }
 }
